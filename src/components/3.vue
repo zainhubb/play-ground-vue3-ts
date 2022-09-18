922073efbaf7528633d5c02f5630708607ec0a47
Zain <1062033146@qq.com>
2022-09-18 17:39:53 +0800

    mod test

--- src/components/3.vue
+++ src/components/3.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div>
     <n-button @click="toggle" type="info">toggle</n-button>
     <demo :age="age"></demo>
@@ -9,11 +9,31 @@
 const age = ref(0)
 const toggle = () => {
   age.value++
-  
 }
 getUser()
 </script>
 
 <style lang="postcss" scoped>
+
+</style> -->
+<template>
+  <div>
+123
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup () {
+    
+
+    return {}
+  }
+})
+</script>
+
+<style scoped lang="postcss">
 
 </style>
