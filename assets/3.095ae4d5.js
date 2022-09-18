49a99dce0b267d1fb5ab5bfae4b9fee1e3c1879d
Zain <1062033146@qq.com>
2022-09-18 17:55:24 +0800

    deploy

---
+++ assets/3.095ae4d5.js
@@ -0,0 +1,1 @@
+import{d as a,f as s,o as t,c as e,t as n}from"./index.58ac5d01.js";const o=a({__name:"3",setup(a){const o=s(0);return(a,s)=>(t(),e("div",null,n(o.value),1))}});export{o as default};
